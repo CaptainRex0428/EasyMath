575c706ba332822169a3794e17c3c2ba63e6112d
Rex <rvo0428@163.com>
2025-10-11 14:43:06 +0800

    #Update add config resh

--- Build.lua
+++ Build.lua
@@ -1,5 +1,6 @@
 include "Directory.lua"
 include "Dependencies.lua"
+include "Config.lua"
 
 workspace "EasyMath"
 	architecture "x64"
@@ -14,6 +15,7 @@ workspace "EasyMath"
     filter "system:windows"
     buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus","/utf-8" }
 	
+	add_rider_refreshconfig()
 
 group ""
 	include "EasyMath.lua"
